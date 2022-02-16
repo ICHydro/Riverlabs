643fb7014a927f6afbb00027bc06c62a71e99a7b
Wouter Buytaert <w.buytaert@imperial.ac.uk>
2022-02-16 21:25:16 +0000

    removing lidarlite dependency from wari_3G

--- wari_3G/Rio_Sensors.h
+++ wari_3G/Rio_Sensors.h
@@ -3,13 +3,11 @@
  */
 
 #include "Rio.h"
-#include "LIDARLite.h"
 
 #ifndef Rio_Sensors_h
 #define Rio_Sensors_h
 
 int16_t readMaxBotix(uint8_t, uint8_t, uint8_t, bool);
-uint8_t readLidarLite(int16_t*, uint8_t, uint8_t, Stream &stream);
 
 int EZread(Stream &stream);
 
