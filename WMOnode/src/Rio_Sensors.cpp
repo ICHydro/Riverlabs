cbcefcfafe01b2826cc8dbe73d700bd04d718c69
Wouter Buytaert <w.buytaert@imperial.ac.uk>
2022-10-16 21:31:01 +0100

    revert Rio_sensors.h and Rio_sensors.cpp for WMOnode

--- WMOnode/src/Rio_Sensors.cpp
+++ WMOnode/src/Rio_Sensors.cpp
@@ -1,81 +1,52 @@
 
 #include "Rio_Sensors.h"
 
+uint8_t readLidarLite(int16_t* readings, uint8_t nreadings, uint8_t debug, Stream &DebugSerial) {
 
-int16_t readMaxBotix(uint8_t serialPin, uint8_t powerPin, uint8_t nreadings, bool debug) {
+    LIDARLite myLidarLite;
+    uint16_t reading;
+    uint8_t i = 0;                     // number of attempts
+    uint8_t j = 0;                     // number of successful readings (> 0)
 
-    int16_t readings[nreadings];
-
-    for (int i = 0; i < nreadings; i++){
-      readings[i] = -1;
-    }
-
-    // select the type of serial on the basis of the serialPin
-
-    if(serialPin == 0) {
-        Serial.begin(9600);
-    }
+    // initialize with 0
+    memset(readings, 0, nreadings * 2); // readings is an int16_t array, so two bytes per position
 
     digitalWrite(WriteLED, HIGH);
-    digitalWrite(MBONPIN, LOW);
-    delay(160);   // wait 160ms for startup and boot message to pass
-    
-    uint32_t readstart = millis();
-    uint8_t n = 0;
-         
-    while ((millis() - readstart) <= 1800) {
-        readings[n] = EZread(Serial);
-        if(readings[n] > -2) {                   // Returning -2 indicates an error
-            n++;
-            if(n >= nreadings) {n = 0; }             //avoid overflow
+    digitalWrite(SWITCH5V, HIGH);  
+    delay(5);
+    digitalWrite(Boost5V_on, HIGH);    // capacitor needs only 10ms to charge but values below 30 seem to affect lidar precision.
+    delay(30);
+    digitalWrite(LIDARONPIN, HIGH);
+    delay(30);
+    myLidarLite.begin(0, false);    
+    reading = myLidarLite.distance();
+    i++;
+    if(reading > 0) {
+        readings[j] = reading;
+        j++;
+    }
+    while((j < nreadings) && (i < 50)) {           // Maximum 50 attempts
+        reading = myLidarLite.distance(false);
+        i++;
+        if(reading > 0) {
+            readings[j++] = reading;
         }
     }
-
-    Serial.end();
-
-    digitalWrite(MBONPIN, HIGH);
+    digitalWrite(LIDARONPIN, LOW);
+    delay(5);
+    digitalWrite(SWITCH5V, LOW);
+    delay(5);
+    digitalWrite(Boost5V_on, LOW);
+    delay(5);
     digitalWrite(WriteLED, LOW);
 
-    // Note: if more than half of the array is not filled because of read errors
-    // then the median will be -1. Fine for now, because this just means that
-    // not enough readings could be taken to return a reliable measurement.
-            
-    int16_t distance = median(readings, nreadings);
-
-    return distance;
-}
-
-/* function copied from the TTL_ArduinoCode_Parsing example on the Arduino forum */
-
-int EZread(Stream &stream) {
-  
-    int result;
-    char inData[6];                             //char array to read data into
-    int index = 0;
-    unsigned long timer;
-    boolean stringComplete = false;
-
-    stream.flush();                           // Clear cache ready for next reading
-    timer = millis();                           // use timer to time out after 1 sec.
-
-    while ((stringComplete == false) && ((millis() - timer) < 1000)) {
-        if (stream.available()) {    
-            char rByte = stream.read();       //read serial input for "R" to mark start of data
-            if(rByte == 'R') {
-                while (index < 5) {             //read next 4 characters for range from sensor
-                    if (stream.available()) {
-                        inData[index] = stream.read();               
-                        index++;                // Increment where to write next
-                    }
-                }
-                inData[index] = 0x00;           //add a padding byte at end for atoi() function
-            }
-            rByte = 0;                          //reset the rByte ready for next reading
-            index = 0;                          // Reset index ready for next reading
-            stringComplete = true;              // Set completion of read to true
-            result = atoi(inData);              // Changes string data into an integer for use
+    if(debug > 1) {
+        Serial.println();
+        for(i = 0; i < nreadings; i++) {
+            Serial.print(readings[i]);
+            Serial.print(" ");
         }
+        Serial.println();
     }
-    if(stringComplete == false) result = -2;
-    return result;
+    return 1;   // success.
 }
--- WMOnode/src/Rio_Sensors.h
+++ WMOnode/src/Rio_Sensors.h
@@ -3,12 +3,11 @@
  */
 
 #include "Rio.h"
+#include "LIDARLite.h"
 
 #ifndef Rio_Sensors_h
 #define Rio_Sensors_h
 
-int16_t readMaxBotix(uint8_t, uint8_t, uint8_t, bool);
-
-int EZread(Stream &stream);
+uint8_t readLidarLite(int16_t*, uint8_t, uint8_t, Stream &stream);
 
 #endif
