e0d920dcba5781747629c292aa5725126cb639c9
Wouter Buytaert <w.buytaert@imperial.ac.uk>
2022-10-31 23:07:04 +0000

    correct XBee reset pin

--- WMOnode/WMOnode.ino
+++ WMOnode/WMOnode.ino
@@ -23,14 +23,14 @@
 #define READ_INTERVAL 5                           // Interval for sensor readings, in minutes
 #define SEND_INTERVAL 1                           // telemetry interval, in hours
 #define NREADINGS 9                               // number of readings taken per measurement (excluding 0 values)
-#define HOST "riverflow.io"                // internet address of the IoT server to report to
-#define ACCESSTOKEN "wNNecsvIMvKrnL1v0G48"               // COAP access token
-#define LOGGERID "RL000328"                               // Logger ID. Set to whatever you like
+#define HOST "demo.thingsboard.io"                // internet address of the IoT server to report to
+#define ACCESSTOKEN "A1_TEST_TOKEN"               // COAP access token
+#define LOGGERID ""                               // Logger ID. Set to whatever you like
 #define APN "hologram"                                    // APN of the cellular network
 #define TIMEOUT 180                               // cellular timeout in seconds, per attempt
 #define DONOTUSEEEPROMSENDBUFFER
 #define NTC                                       // set the clock at startup by querying an ntc server
-#define OPTIBOOT                                  // set ONLY if your device uses the optiboot bootloader. Enables the watchdog timer
+//#define OPTIBOOT                                  // set ONLY if your device uses the optiboot bootloader. Enables the watchdog timer
 
 /*************** includes ******************/
 
