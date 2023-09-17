e4174b7a99e826aa4a18e032535e91e291e986ac
Wouter Buytaert <w.buytaert@imperial.ac.uk>
2023-09-17 22:47:51 +0100

    removing stale code

--- WMOnode/WMOnode.ino
+++ WMOnode/WMOnode.ino
@@ -23,10 +23,10 @@
 #define READ_INTERVAL 5                           // Interval for sensor readings, in minutes
 #define SEND_INTERVAL 1                           // telemetry interval, in hours
 #define NREADINGS 9                               // number of readings taken per measurement (excluding 0 values)
-#define HOST "riverflow.io"                // internet address of the IoT server to report to
-#define ACCESSTOKEN "dnupCrnsrJC3zduojbf7"               // COAP access token
-#define LOGGERID "RL000377"                               // Logger ID. Set to whatever you like
-#define APN "arkessalp.com"                                    // APN of the cellular network
+#define HOST "demo.thingsboard.io"                // internet address of the IoT server to report to
+#define ACCESSTOKEN "A1_TEST_TOKEN"               // COAP access token
+#define LOGGERID ""                               // Logger ID. Set to whatever you like
+#define APN ""                                    // APN of the cellular network
 #define TIMEOUT 180                               // cellular timeout in seconds, per attempt
 #define DONOTUSEEEPROMSENDBUFFER
 #define NTC                                       // set the clock at startup by querying an ntc server
