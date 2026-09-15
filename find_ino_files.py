828cfa80a82fe8d1b5e8b3f33449c9b36ceeb6e7
Steph Wills <stephanie.wills13@imperial.ac.uk>
2026-09-15 10:10:40 +0100

    Add comments for clarity

--- find_ino_files.py
+++ find_ino_files.py
@@ -1,23 +1,29 @@
 import glob
 import os
 
-Import("env")
+Import("env") # import the active SCons environment
+
 
 def FindInoNodes(env):
-    """Find all .ino files within the sensor directory for the active environment.
+    """Return all .ino nodes within a sensor directory.
+
+    PlatformIO uses the software construction tool SCons. In SCons, files are represented
+    as 'nodes', which SCons uses to track files involved in the build. This function
+    returns the .ino files for a sensor as SCons nodes.
 
     Args:
         env: The active SCons build environment.
 
     Returns:
-        list: A list of SCons nodes representing the .ino files in the sensor directory.
+        list: A list of SCons nodes representing the .ino files for a sensor.
     """
     project_root = env.subst("$PROJECT_DIR")
 
-    # read from platform.ini for active env
+    # custom_sensor_dir is read from platformio.ini for the active env
     sensor_dir = env.GetProjectOption("custom_sensor_dir")
     full_sensor_dir = glob.escape(os.path.join(project_root, sensor_dir))
 
+    # env.Glob() returns SCons nodes for the matching .ino files
     ino_nodes = []
     ino_nodes += env.Glob(os.path.join(full_sensor_dir, "*.ino"))
 
